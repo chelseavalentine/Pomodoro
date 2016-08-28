ef7beca645df621baf5f4938920d686dbbfcf6e5
Chelsea Valentine <cv851@nyu.edu>
2016-08-28 07:52:58 -0400

    Initial commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Chelsea Valentine on 8/28/16.
+//  Copyright © 2016 Chelsea Valentine. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
