4d98c21d2cbe24549933a4416ae422b0fdedde2e
Chelsea Valentine <cv851@nyu.edu>
2016-08-31 08:58:09 -0400

    TextField styling. Enter listener

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,20 +9,62 @@
 import Cocoa
 
 class ViewController: NSViewController {
+
+    @IBOutlet weak var focusTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Initialize event listeners
+        NSEvent.addLocalMonitorForEventsMatchingMask(.KeyUpMask) { (aEvent) -> NSEvent! in
+            self.keyUp(aEvent)
+            return aEvent
+        }
+
     }
 
     override var representedObject: AnyObject? {
         didSet {
+            
         // Update the view, if already loaded.
         }
     }
 
     override func awakeFromNib() {
         self.view.window?.backgroundColor = NSColor.init(red: 32/255, green: 34/255, blue: 38/255, alpha: 1.0)
+    
+        focusTextField.textColor = NSColor.whiteColor()
+        
+        // Set TextField font and color
+        let placeholderTextColor = NSColor(red: 255, green: 255, blue: 255, alpha: 0.65)
+        let placeholderFont: NSFont? = NSFont(name: "PT Mono", size: 12)
+        let placeholderAttributes: [String: AnyObject] = [
+            NSForegroundColorAttributeName: placeholderTextColor,
+            NSFontAttributeName: placeholderFont!
+        ]
+        let placeholderAttributedString = NSAttributedString(string: "What are you going to do?", attributes: placeholderAttributes)
+        focusTextField.placeholderAttributedString = placeholderAttributedString
+        
+        // Make TextField caret white
+        let fieldEditor = self.view.window?.fieldEditor(true, forObject: self) as? NSTextView
+        fieldEditor?.insertionPointColor = NSColor.whiteColor()
+    }
+
+    override func keyUp(theEvent: NSEvent) {
+        print("hahaha")
+        // Return was pressed
+        if (theEvent.keyCode == 36) {
+            print("hell yea")
+            if (focusTextField!.stringValue == "") {
+                print("Shit's empty")
+            } else {
+                print("Great")
+            }
+            
+            print(focusTextField!.stringValue)
+        }
     }
 }
 
