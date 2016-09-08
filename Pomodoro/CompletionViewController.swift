327279b3422d19252b8908f7f749904cd27b9d4d
Chelsea Valentine <cv851@nyu.edu>
2016-09-07 21:32:43 -0400

    Confetti animation

--- Pomodoro/CompletionViewController.swift
+++ Pomodoro/CompletionViewController.swift
@@ -11,10 +11,10 @@ import Cocoa
 class CompletionViewController: NSViewController {
     @IBOutlet weak var confettiView: NSView!
     let helper = Helper.sharedInstance
+    var endTimer: NSTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         generateConfetti()
     }
     
@@ -23,7 +23,7 @@ class CompletionViewController: NSViewController {
     }
     
     func generateConfetti() {
-        let NUM_STARS = 24
+        let NUM_STARS = 36
         var confetti: NSImageView?
         var confettiNames: [String] = ["blueConfetti", "greenConfetti", "yellowConfetti"]
         var genericFrame = NSRect(x: 0, y: 0, width: 8, height: 8)
@@ -34,9 +34,34 @@ class CompletionViewController: NSViewController {
             confetti = NSImageView(frame: genericFrame)
             confetti!.image = NSImage(named: confettiNames[Int(arc4random_uniform(UInt32(confettiNames.count)))])
             confetti!.frame = genericFrame
-            print(confetti!.image)
-            print(confetti!.frame)
             confettiView.addSubview(confetti!)
+        
+            NSTimer.scheduledTimerWithTimeInterval(0.0000001, target: self, selector: #selector(CompletionViewController.floatDown), userInfo: confetti, repeats: true)
         }
+        
+        endTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(CompletionViewController.endAnimation), userInfo: nil, repeats: true)
+    }
+    
+    func floatDown(timer: NSTimer) {
+        let image: NSImageView = timer.userInfo as! NSImageView
+        
+        if (image.frame.origin.y >  0 - image.frame.height) {
+//            if (arc4random_uniform(10) == 0) {
+//                image.frame.origin.x += CGFloat(-3 + Int(arc4random_uniform(6)))
+//            } else {
+//            image.frame.origin.y -= CGFloat(arc4random_uniform(20))
+            image.frame.origin.y -= 20
+//            }
+        } else {
+            image.removeFromSuperview()
+            timer.invalidate()
+        }
+    }
+    
+    func endAnimation(timer: NSTimer) {
+        timer.invalidate()
+        
+        let nextViewController = self.storyboard?.instantiateControllerWithIdentifier("ResultsViewController") as? ResultsViewController
+        self.view.window?.contentViewController = nextViewController
     }
 }
